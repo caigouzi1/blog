5c9c3f3aa5bef5abd928c452d050083df77e8229
caigouzi1 <759160301@qq.com>
2024-05-25 15:45:16 +0000

    Deploying to gh-pages from  @ 0385cc1a88dbbcdd8bfa44edea63380d9570e270 🚀

---
+++ assets/sql.html-CHRkyNHX.js
@@ -0,0 +1,20 @@
+import{_ as s,c as a,o as n,a as e}from"./app-DaJ1Kvbf.js";const l={},t=e(`<h1 id="sql使用" tabindex="-1"><a class="header-anchor" href="#sql使用"><span>SQL使用</span></a></h1><h2 id="导出mysql数据库中的表结构" tabindex="-1"><a class="header-anchor" href="#导出mysql数据库中的表结构"><span>导出MySQL数据库中的表结构</span></a></h2><div class="language-sql line-numbers-mode" data-ext="sql" data-title="sql"><pre class="language-sql"><code><span class="token keyword">SELECT</span>
+ COLUMN_NAME 列名<span class="token punctuation">,</span>
+ COLUMN_TYPE 数据类型<span class="token punctuation">,</span>
+ DATA_TYPE 字段类型<span class="token punctuation">,</span>
+ <span class="token keyword">IF</span><span class="token punctuation">(</span>IS_NULLABLE<span class="token operator">=</span><span class="token string">&#39;NO&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;否&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;是&#39;</span><span class="token punctuation">)</span> 是否为空<span class="token punctuation">,</span>
+ COLUMN_DEFAULT 默认值<span class="token punctuation">,</span>
+ COLUMN_COMMENT 备注<span class="token punctuation">,</span>
+ TABLE_NAME  表名
+<span class="token keyword">FROM</span>
+  INFORMATION_SCHEMA<span class="token punctuation">.</span><span class="token keyword">COLUMNS</span> 
+<span class="token keyword">WHERE</span> 
+  TABLE_SCHEMA <span class="token operator">=</span> <span class="token string">&#39;数据库名称&#39;</span> 
+<span class="token operator">AND</span> TABLE_NAME <span class="token operator">=</span> <span class="token string">&#39;您的表名称&#39;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>将查询结果导出即可。</p><blockquote><p>MySQL的INFORMATION_SCHEMA为系统默认库，存储数据库相关信息。<br> COLUMNS表： 存储设置的列的相关信息。</p></blockquote><h2 id="mysql数据备份和恢复" tabindex="-1"><a class="header-anchor" href="#mysql数据备份和恢复"><span>MySQL数据备份和恢复</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token comment"># 备份</span>
+mysqldump <span class="token parameter variable">-u</span> 用户名 <span class="token parameter variable">-p</span> 数据库名 <span class="token operator">&gt;</span> 备份文件名.sql
+mysqldump <span class="token parameter variable">-u</span> 用户名 <span class="token parameter variable">-p</span> 数据库名 表名 <span class="token operator">&gt;</span> 备份文件名.sql
+
+<span class="token comment"># 恢复</span>
+mysql -u用户名 <span class="token parameter variable">-p</span> 数据库名 <span class="token operator">&lt;</span> 备份文件名.sql
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7),i=[t];function p(c,o){return n(),a("div",null,i)}const d=s(l,[["render",p],["__file","sql.html.vue"]]),u=JSON.parse('{"path":"/manual/sql.html","title":"SQL使用","lang":"zh-CN","frontmatter":{"date":"2023-08-15T00:00:00.000Z","category":"开发者手册","tag":["SQL"]},"headers":[{"level":2,"title":"导出MySQL数据库中的表结构","slug":"导出mysql数据库中的表结构","link":"#导出mysql数据库中的表结构","children":[]},{"level":2,"title":"MySQL数据备份和恢复","slug":"mysql数据备份和恢复","link":"#mysql数据备份和恢复","children":[]}],"git":{"createdTime":1716651784000,"updatedTime":1716651784000},"readingTime":{"minutes":0.6,"words":179},"filePathRelative":"manual/sql.md","localizedDate":"2023年8月15日"}');export{d as comp,u as data};
---
+++ assets/wsl.html-BR6Xutln.js
@@ -0,0 +1,20 @@
+import{_ as s,c as a,o as e,a as n}from"./app-DaJ1Kvbf.js";const l={},i=n(`<h1 id="wsl开发" tabindex="-1"><a class="header-anchor" href="#wsl开发"><span>WSL开发</span></a></h1><h2 id="相关博客" tabindex="-1"><a class="header-anchor" href="#相关博客"><span>相关博客</span></a></h2><ol><li><a href="https://zhuanlan.zhihu.com/p/122214146" target="_blank" rel="noopener noreferrer">Linux系统各发行版换国内yum或apt源，加速软件下载更新</a></li></ol><blockquote><p><a href="https://learn.microsoft.com/zh-cn/windows/wsl/basic-commands" target="_blank" rel="noopener noreferrer">微软官方文档</a></p></blockquote><h2 id="常用命令" tabindex="-1"><a class="header-anchor" href="#常用命令"><span>常用命令</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token comment"># 列出可用的 Linux 发行版</span>
+wsl <span class="token parameter variable">--list</span> <span class="token parameter variable">--online</span>
+
+<span class="token comment"># 列出已安装的 Linux 发行版</span>
+wsl <span class="token parameter variable">--list</span> <span class="token parameter variable">--verbose</span>
+
+<span class="token comment"># 将 WSL 版本设置为 1 或 2</span>
+wsl --set-version <span class="token operator">&lt;</span>distribution name<span class="token operator">&gt;</span> <span class="token operator">&lt;</span>versionNumber<span class="token operator">&gt;</span>
+
+<span class="token comment"># 设置默认 WSL 版本</span>
+wsl --set-default-version <span class="token operator">&lt;</span>Version<span class="token operator">&gt;</span>
+
+<span class="token comment"># 设置默认 Linux 发行版</span>
+wsl --set-default <span class="token operator">&lt;</span>Distribution Name<span class="token operator">&gt;</span>
+
+<span class="token comment"># 注销或卸载 Linux 发行版</span>
+wsl <span class="token parameter variable">--unregister</span> <span class="token operator">&lt;</span>DistributionName<span class="token operator">&gt;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="vscode的使用" tabindex="-1"><a class="header-anchor" href="#vscode的使用"><span>VsCode的使用</span></a></h2><ol><li><p>vim ~/.bashrc添加环境变量</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token builtin class-name">alias</span> <span class="token assign-left variable">code</span><span class="token operator">=</span><span class="token string">&quot;/mnt/d/code/Microsoft\\ VS\\ Code/Code.exe&quot;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li><p>使设置生效</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token builtin class-name">source</span> ~/.bashrc
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li></ol>`,8),t=[i];function r(o,c){return e(),a("div",null,t)}const p=s(l,[["render",r],["__file","wsl.html.vue"]]),m=JSON.parse('{"path":"/manual/wsl.html","title":"WSL开发","lang":"zh-CN","frontmatter":{"date":"2020-12-14T00:00:00.000Z","category":"开发者手册","tag":["wsl"]},"headers":[{"level":2,"title":"相关博客","slug":"相关博客","link":"#相关博客","children":[]},{"level":2,"title":"常用命令","slug":"常用命令","link":"#常用命令","children":[]},{"level":2,"title":"VsCode的使用","slug":"vscode的使用","link":"#vscode的使用","children":[]}],"git":{"createdTime":1716651784000,"updatedTime":1716651784000},"readingTime":{"minutes":0.55,"words":165},"filePathRelative":"manual/wsl.md","localizedDate":"2020年12月14日"}');export{p as comp,m as data};
